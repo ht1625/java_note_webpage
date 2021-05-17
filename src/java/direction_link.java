3f295275e1e30860b53f62b0e070f68a6f73b845
ht1625 <61890580+ht1625@users.noreply.github.com>
2021-05-17 19:29:00 +0300

    dsfghj

---
+++ src/java/direction_link.java
@@ -0,0 +1,51 @@
+
+import javax.faces.bean.SessionScoped;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+@SessionScoped
+
+public class direction_link extends db_connect implements Serializable {
+
+    direction_link() throws Exception {
+    }
+
+    public String back_homepage() {
+        return "Home.xhtml";
+    }
+
+    public String back_genelnot() {
+        return "genel_not.xhtml";
+    }
+
+    public String back_todolist() {
+        return "todo_list.xhtml";
+    }
+
+    public String back_haftalık() {
+        return "haftalık.xhtml";
+    }
+
+    public String shownote_genel() {
+        //burdan bilgiyi alıp gidecek sayfaya
+        return "show_notes.xhtml";
+    }
+
+    public String shownote_list() {
+        //burdan bilgiyi alıp gidecek sayfaya
+        return "show_notes.xhtml";
+    }
+
+    public String shownote_haftalık() {
+        //burdan bilgiyi alıp gidecek sayfaya
+        return "show_notes.xhtml";
+    }
+
+    public String shownote_cop() {
+        //burdan bilgiyi alıp gidecek sayfaya
+        return "show_notes.xhtml";
+    }
+}
